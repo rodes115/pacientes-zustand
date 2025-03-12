af36e08afaafb0da4923759cf4d9d688364ebc2f
rodes115 <rodesreyes115@gmail.com>
2025-03-11 23:49:16 -0600

    cap-247

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,13 +1,16 @@
 import { useForm } from "react-hook-form"
 import Error from "./Error"
 import type { DraftPatient } from "../types"
+import { usePatientStore } from "../store"
 
 export default function PatientForm() {
+
+     const { addPatient } = usePatientStore()
 
      const { register, handleSubmit, formState: { errors } } = useForm<DraftPatient>()
 
      const registerPatient = (data : DraftPatient) => {
-          console.log(data)
+          addPatient(data)
      }
 
      return (
--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,5 +1,9 @@
+import { usePatientStore } from "../store"
 
 export default function PatientList() {
+     const patients = usePatientStore(state =>state.patients)
+     
+     console.log(patients)
      return (
           <div>PatientList</div>
      )
